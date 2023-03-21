fc4f7376760e99208cb6d798f9d1eee70956d46a
Sterre Govaert <sterre.govaert@student.hogent.be>
2023-03-21 15:58:50 +0100

    Speler klasse aangepast: geboortedatum => geboortejaar, ...

--- src/domein/Speler.java
+++ src/domein/Speler.java
@@ -1,82 +1,61 @@
 package domein;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.Objects;
-
 public class Speler {
 
-	private String gebruikersNaam;
-	private LocalDate geboortejaar;
-	
-	/**
-	 * 
-	 * @param gebruikersNaam
-	 * @param geboorteJaar
-	 */
-	public Speler(String gebruikersNaam, int geboortejaar) {
-		setGebruikersNaam(gebruikersNaam);
-		setGeboorteDatumEnJaar(geboorteDatum);
+	private String gebruikersnaam;
+	private int geboortejaar;
+
+	public Speler(String gebruikersnaam, int geboortejaar) {
+		setGebruikersnaam(gebruikersnaam);
+		setGeboortejaar(geboortejaar);
 	}
 
-	public String getGebruikersNaam() {
-		return this.gebruikersNaam;
+	public String getGebruikersnaam() {
+		return this.gebruikersnaam;
 	}
 
-	private void setGebruikersNaam(String gebruikersNaam) {
-		//naam moet ingevuld zijn
-		if(gebruikersNaam == null || gebruikersNaam.isBlank())
-			throw new IllegalArgumentException("GebruikersNaam moet ingevuld zijn.");
-		
+	private void setGebruikersnaam(String gebruikersNaam) {
+		if (gebruikersNaam == null || gebruikersNaam.isBlank()) {
+			throw new IllegalArgumentException("Gebruikersnaam moet ingevuld zijn.");
+		}
+
 		String geldigeTekens = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZéèö0123456789 _";
 		String alphabet = geldigeTekens.substring(0, geldigeTekens.indexOf("0"));
 		boolean bevatOngeldigeTeken = false;
-		
-		for (int i = 0; i < gebruikersNaam.length(); i++) {
-			if (geldigeTekens.indexOf(gebruikersNaam.charAt(i)) == -1) bevatOngeldigeTeken = true;
+
+		for (int i = 0; i < gebruikersnaam.length(); i++) {
+			if (geldigeTekens.indexOf(gebruikersnaam.charAt(i)) == -1)
+				bevatOngeldigeTeken = true;
 		}
-		//naam mag geen ongeldige tekens bevatten
-		if(bevatOngeldigeTeken)
-			throw new IllegalArgumentException("GebruikersNaam mag enkel letters en cijfers en spaties en underscores('_') bevatten.");
-		//naam moet starten met een letter
-		if(alphabet.indexOf(gebruikersNaam.charAt(0)) == -1)
+		// naam mag geen ongeldige tekens bevatten
+		if (bevatOngeldigeTeken) {
+			throw new IllegalArgumentException(
+					"Gebruikersnaam mag enkel letters en cijfers en spaties en underscores('_') bevatten.");
+		}
+
+		// naam moet starten met een letter
+		if (alphabet.indexOf(gebruikersnaam.charAt(0)) == -1) {
 			throw new IllegalArgumentException("GebruikersNaam moet starten met een letter.");
-		this.gebruikersNaam = gebruikersNaam;
+		}
+		this.gebruikersnaam = gebruikersnaam;
 	}
 
-	public LocalDate getGeboorteDatum() {
-		return this.geboorteDatum;
-	}
-	
-	private void setLeeftijd(int leeftijdInJaar) {
-		this.leeftijd = leeftijdInJaar;
-	}
-	
-	public int getLeeftijd() {
-		return this.leeftijd;
+	public int getGeboortejaar() {
+		return this.geboortejaar;
 	}
 
-	private void setGeboorteDatumEnJaar(LocalDate geboorteDatum) {
-		LocalDate huidigeTijd = LocalDate.now();
-		
-		//de tijd tussen de geboorte dag/maand/jaar en vandaag
-		Period period = Period.between(geboorteDatum, huidigeTijd);
-		if(period.getYears() < 6)
+	private void setGeboortejaar(int geboortejaar) {
+		if (geboortejaar > 2016) {
 			throw new IllegalArgumentException("Je moet minstens 6 jaar oud zijn om dit spel te spelen.");
-		int leeftijdInJaar = period.getYears();
-		this.geboorteDatum = geboorteDatum;
-		setLeeftijd(leeftijdInJaar);
+		}
+		this.geboortejaar = geboortejaar;
 	}
-	
 
-	//toString toegevoegd
 	@Override
 	public String toString() {
-		int leeftijdinJaar = LocalDate.now().getYear() - leeftijd;
-		
-		return String.format("%s: %s ---- leeftijd: %d", getClass().getSimpleName(), gebruikersNaam, leeftijd);
+		int leeftijdInJaar = 2023 - this.geboortejaar;
+
+		return String.format("%s: %s --- leeftijd: %d", getClass().getSimpleName(), gebruikersnaam, leeftijdInJaar);
 	}
-
-
 
 }
