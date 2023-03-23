89ea9f01cd76bd2940401854d633043c92d8abe1
Sterre Govaert <sterre.govaert@student.hogent.be>
2023-03-23 13:32:52 +0100

    spelerTest
    extra testen toegevoegd en een aantal verbeterd

--- src/testen/SpelerTest.java
+++ src/testen/SpelerTest.java
@@ -15,44 +15,49 @@ class SpelerTest {
 
 	Speler s, s2, s3, s4;
 
-	static final String spelerNaamOk1 = "speler1";
-	static final String spelerNaamOk2 = "Speler_Naam 2";
+	static final String GELDIGE_NAAM = "speler1";
+	static final String GELDIGE_NAAM_2 = "Speler_Naam 2";
 	static final int GELDIGE_LEEFTIJD = 2000;
 	static final int ONGELDIGE_LEEFTIJD = 2022;
 
-	//TODO eventueel in parameterized test van maken met geldige namen in de plaats
 	@Test
 	void maakSpeler_geldigeParam_MaaktSpeler() {
-		Speler s = new Speler(spelerNaamOk1, GELDIGE_LEEFTIJD);
-		Speler s2 = new Speler(spelerNaamOk2, GELDIGE_LEEFTIJD);
+		Speler s = new Speler(GELDIGE_NAAM, GELDIGE_LEEFTIJD);
+		Speler s2 = new Speler(GELDIGE_NAAM_2, GELDIGE_LEEFTIJD);
 
 		assertEquals("speler1", s.getGebruikersnaam());
 		assertEquals("Speler_Naam 2", s2.getGebruikersnaam());
 	}
 
 	@ParameterizedTest
-	@NullAndEmptySource
-	@ValueSource(strings = { "", "   ", " \t   \n" })
-	void maakSpeler_legeOfSpatiesBevatendeOngeldigeGebruikersnaam_WerptException(String ongeldigeNaam) {
-		assertThrows(IllegalArgumentException.class, () -> new Speler(ongeldigeNaam, GELDIGE_LEEFTIJD));
+	@ValueSource(strings = { "speler3", "S123456", "S 1234_", "s___ 123" })
+	void maakSpeler_geldigeGebruikersnamen_maaktDeSpelersAan(String geldigeNaam) {
+		assertThrows(IllegalArgumentException.class, () -> new Speler(geldigeNaam, GELDIGE_LEEFTIJD));
 	}
-	//TODO deze kan opgesplist worden in met speciale tekens en met started met een cijfer
+
 	@ParameterizedTest
 	@NullAndEmptySource
+	@ValueSource(strings = { "", "   ", " \t   \n" })
+	void maakSpeler_legeOfSpatiesBevatendeOngeldigeGebruikersnaam_werptException(String legeNaam) {
+		assertThrows(IllegalArgumentException.class, () -> new Speler(legeNaam, GELDIGE_LEEFTIJD));
+	}
+
+	@ParameterizedTest
 	@ValueSource(strings = { "8SpelerNaam", "s%%%", "abcdefg[][^§(((" })
-	void maakSpeler_OngeldigeGebruikersnaam_WerptException(String ongeldigeNaam) {
+	void maakSpeler_OngeldigeGebruikersnaam_werptException(String ongeldigeNaam) {
 		assertThrows(IllegalArgumentException.class, () -> new Speler(ongeldigeNaam, GELDIGE_LEEFTIJD));
 	}
 
 	@Test
-	void maakSpeler_spelerTeJong_WerptException() {
-		assertThrows(IllegalArgumentException.class, () -> new Speler(spelerNaamOk1, ONGELDIGE_LEEFTIJD));
+	void maakSpeler_spelerTeJong_werptException() {
+		assertThrows(IllegalArgumentException.class, () -> new Speler(GELDIGE_NAAM, ONGELDIGE_LEEFTIJD));
 	}
 
-	//TODO huidig jaar oproepen en dan -6 doen
 	@Test
-	void maakSpeler_spelerNetTeJong_WerptException() {
-		assertThrows(IllegalArgumentException.class, () -> new Speler(spelerNaamOk1, 2018));
+	void maakSpeler_spelerNetTeJong_werptException() {
+		LocalDate now = LocalDate.now();
+		int geboortejaarTeJongeSpeler = now.getYear() - 5;
+		assertThrows(IllegalArgumentException.class, () -> new Speler(GELDIGE_NAAM, geboortejaarTeJongeSpeler));
 	}
 
 }
