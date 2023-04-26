a5f1178e10ceda5f2c528c6d4e3c7b3879e326b8
Peter Cypers <peter.cypers@student.hogent.be>
2023-04-26 12:33:50 +0200

    switch StartUp gemaakt met keuze om met Cui of Gui te starten
    ook voorlopige module info aangemaakt die geen errors geeft, tijdelijke
    lege GUI start met Launch methode, afwachtend op zij - branch GUI?
    invulling

---
+++ src/main/StartUp.java
@@ -0,0 +1,54 @@
+package main;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import cui.SplendorApplicatie;
+import domein.DomeinController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StartUp extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO GUI opbouw
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int keuze = 0;
+		boolean errorExists = true;
+		do {
+		try {
+			do {
+				System.out.print("Gui?(1) of Cui?(2): ");
+				keuze = input.nextInt();
+			}while(keuze < 1 || keuze > 2);
+			errorExists = false;
+		} catch (InputMismatchException e) {
+			input.next();
+			System.out.println("Je keuze moet een geheel getal zijn.");
+		}
+		}while(errorExists);
+		
+		//CUI opstarten
+		if(keuze == 2) {
+			new SplendorApplicatie(new DomeinController()).startSpel();
+			System.exit(0);
+		}
+		//GUI opstarten
+		if(keuze == 1) {
+			try {
+			launch(args);
+			}catch(Error e) {
+				System.exit(1);
+			}
+		}
+
+	}
+
+
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,21 @@
+module splendorModule {
+	exports persistentie;
+	exports cui;
+	exports main;
+	exports domein;
+	exports testen;
+	exports dto;
+
+	requires java.sql;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires javafx.controls;
+	requires org.junit.jupiter.api;
+	requires org.junit.jupiter.params;
+	
+    //exports main to javafx.graphics; //auto-generated exports main -> duplicate
+	
+	//scenebuilder:
+	//requires javafx.fxml;
+	//opens gui to javafx.fxml;
+}
