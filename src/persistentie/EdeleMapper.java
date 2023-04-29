dad810724a3f1081a85a65e930754a243a8ed821
Peter Cypers <peter.cypers@student.hogent.be>
2023-04-29 18:25:07 +0200

    OntwikkelingskaartMapper en EdelenMapper:
    fotos van ontwikkelingskaarten en edelen (achtergronden) zijn nu de
    adressen van de fotobestanden met de juiste achtergronden
    volgens de gegeven pdf voorbeeld

--- src/persistentie/EdeleMapper.java
+++ src/persistentie/EdeleMapper.java
@@ -15,8 +15,19 @@ public class EdeleMapper {
 		 * WIT,ROOD,BLAUW,GROEN,ZWART;
 		 * Edele(int pp, String foto, int[] kosten)*/
 		
+		/* foto volgorde: zoals op extra_info gegeven pdf
+		 * noble04, noble10, noble01, noble02, noble09, noble03, noble06, noble07, noble08, noble05 */
 		List<Edele> edelen = new ArrayList<>();
-		String[] fotos = {"dame1", "heer1", "dame2", "dame3", "heer2", "dame4", "heer3", "heer4", "heer5", "dame5"};
+		String[] edelenFotos = {"src\\img\\nobles\\noble04.png",
+								"src\\img\\nobles\\noble10.png",
+								"src\\img\\nobles\\noble01.png",
+								"src\\img\\nobles\\noble02.png",
+								"src\\img\\nobles\\noble09.png",
+								"src\\img\\nobles\\noble03.png",
+								"src\\img\\nobles\\noble06.png",
+								"src\\img\\nobles\\noble07.png",
+								"src\\img\\nobles\\noble08.png",
+								"src\\img\\nobles\\noble05.png"};
 		int[][] kosten = {{4, 0, 0, 0, 4},
 		                  {3, 3, 0, 0, 3},
 		                  {3, 0, 3, 0, 3},
@@ -30,7 +41,7 @@ public class EdeleMapper {
 		
 		//alle edelen aanmaken + toevoegen aan de lijst
 		for (int i = 0; i < kosten.length; i++) {
-			edelen.add(new Edele(EDELENPRESTIGE, fotos[i], kosten[i]));
+			edelen.add(new Edele(EDELENPRESTIGE, edelenFotos[i], kosten[i]));
 		}
 		return edelen;
 	}
--- src/persistentie/OntwikkelingskaartMapper.java
+++ src/persistentie/OntwikkelingskaartMapper.java
@@ -8,13 +8,17 @@ import domein.Ontwikkelingskaart;
 
 public class OntwikkelingskaartMapper {
 	
-	//fotoOntwikkelingskaart TODO - hoe gaan die fotos ingevuld worden?
-	private static final String PLACEHOLDER = "999";
 	public List<Ontwikkelingskaart> geefN1Kaarten(){
 		Kleur kleur;
 		List<Ontwikkelingskaart> niveau1 = new ArrayList<>();
-	
-	/*WIT,ROOD,BLAUW,GROEN,ZWART;*/
+		String[] niveau1Fotos = {"src\\img\\cards\\level1\\mine_diamond.jpg",
+								 "src\\img\\cards\\level1\\mine_ruby.jpg",
+								 "src\\img\\cards\\level1\\mine_sapphire.jpg",
+								 "src\\img\\cards\\level1\\mine_emerald.jpg",
+								 "src\\img\\cards\\level1\\mine_onyx.jpg"};
+		
+	/* WIT,ROOD,BLAUW,GROEN,ZWART */
+	/* DIAMOND, RUBY, SAPPHIRE, EMERALD, ONYX */
 	//niveau 1: index[0][] krijgt telkens 1 prestigepunt
 	int[][] witteKaartKosten = {{0, 0, 4, 0, 0},
 								{0, 0, 0, 3, 0},
@@ -62,15 +66,15 @@ public class OntwikkelingskaartMapper {
 	for (int i = 0; i < witteKaartKosten.length; i++) {
 		int prestigepunten = i==0? 1 : 0;
 		kleur = Kleur.WIT;
-		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, PLACEHOLDER, witteKaartKosten[i]));
+		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, niveau1Fotos[0], witteKaartKosten[i]));
 		kleur = Kleur.ROOD;
-		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, PLACEHOLDER, rodeKaartKosten[i]));
+		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, niveau1Fotos[1], rodeKaartKosten[i]));
 		kleur = Kleur.BLAUW;
-		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, PLACEHOLDER, blauweKaartKosten[i]));
+		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, niveau1Fotos[2], blauweKaartKosten[i]));
 		kleur = Kleur.GROEN;
-		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, PLACEHOLDER, groeneKaartKosten[i]));
+		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, niveau1Fotos[3], groeneKaartKosten[i]));
 		kleur = Kleur.ZWART;
-		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, PLACEHOLDER, zwarteKaartKosten[i]));
+		niveau1.add(new Ontwikkelingskaart(1, prestigepunten, kleur, niveau1Fotos[4], zwarteKaartKosten[i]));
 	}
 	
 	
@@ -82,59 +86,80 @@ public class OntwikkelingskaartMapper {
 		List<Ontwikkelingskaart> niveau2 = new ArrayList<>();
 		
 		// niveau2
-		/* WIT,ROOD,BLAUW,GROEN,ZWART; */
+		/* WIT,ROOD,BLAUW,GROEN,ZWART */
+		/* DIAMOND, RUBY, SAPPHIRE, EMERALD, ONYX */
 		int[] prestigeLijst = {1, 1, 2, 2, 2, 3};
-		String[] fotoLijst = {"1", "1", "2", "1", "2", "2"};
-		String[] fotoBuitenPatroon = {PLACEHOLDER, PLACEHOLDER, PLACEHOLDER, "2", "1", PLACEHOLDER}; //zie excel file
+		String[] patroon1 = {"1", "1", "2", "1", "2", "2"}; //zie excel file
+		String[] patroon2 = {"1", "1", "2", "2", "1", "2"}; //zie excel file
 		
-		
+		/* WIT : DIAMOND  (patroon 1)*/
 		int[][] witteKaartKosten = {{ 0, 2, 0, 3, 2 },
 				                    { 2, 3, 3, 0, 0 },
 				                    { 0, 5, 0, 0, 0 },
 				                    { 0, 4, 0, 1, 2 },
 				                    { 0, 5, 0, 0, 3 },
 				                    { 6, 0, 0, 0, 0 }};
+		String witteKaart1 = "src\\img\\cards\\level2\\caravan_diamond.jpg";
+		String witteKaart2 = "src\\img\\cards\\level2\\lapidary_diamond.jpg";
+		String[] witteKaartFotos = {witteKaart1, witteKaart1, witteKaart2, witteKaart1, witteKaart2, witteKaart2};
+		
+		/* ROOD : RUBY  (patroon 1) */ 
 		int[][] rodeKaartKosten = {{ 2, 2, 0, 0, 3 },
                                    { 0, 2, 3, 0, 3 },
                                    { 0, 0, 0, 0, 5 },
                                    { 1, 0, 4, 2, 0 },
                                    { 3, 0, 0, 0, 5 },
                                    { 0, 6, 0, 0, 0 }};
+		String rodeKaart1 = "src\\img\\cards\\level2\\felucca_ruby.jpg";
+		String rodeKaart2 = "src\\img\\cards\\level2\\lapidary_ruby.jpg";
+		String[] rodeKaartFotos = {rodeKaart1, rodeKaart1, rodeKaart2, rodeKaart1, rodeKaart2, rodeKaart2};
+		
+		/* BLAUW : SAPPHIRE (patroon 2) */
 		int[][] blauweKaartKosten = {{ 0, 3, 2, 2, 0 },
                                      { 0, 0, 2, 3, 3 },
                                      { 0, 0, 5, 0, 0 },
                                      { 2, 1, 0, 0, 4 },
                                      { 5, 0, 3, 0, 0 },
                                      { 0, 0, 6, 0, 0 }};
+		String blauweKaart1 = "src\\img\\cards\\level2\\caravan_sapphire.jpg";
+		String blauweKaart2 = "src\\img\\cards\\level2\\lapidary_sapphire.jpg";
+		String[] blauweKaartFotos = {blauweKaart1, blauweKaart1, blauweKaart2, blauweKaart2, blauweKaart1, blauweKaart2};
+		
+		/* GROEN : EMERALD (patroon 1) */
 		int[][] groeneKaartKosten = {{ 2, 0, 3, 0, 2 },
                                      { 3, 3, 0, 2, 0 },
                                      { 0, 0, 0, 5, 0 },
                                      { 4, 0, 2, 0, 1 },
                                      { 0, 0, 5, 3, 0 },
                                      { 0, 0, 0, 6, 0 }};
+		String groeneKaart1 = "src\\img\\cards\\level2\\trader_emerald.jpg";
+		String groeneKaart2 = "src\\img\\cards\\level2\\carrack_emerald.jpg";
+		String[] groeneKaartFotos = {groeneKaart1, groeneKaart1, groeneKaart2, groeneKaart1, groeneKaart2, groeneKaart2};
+		
+		/* ZWART : ONYX (patroon 1) */
 		int[][] zwarteKaartKosten = {{ 3, 0, 2, 2, 0 },
                                      { 3, 0, 0, 3, 2 },
                                      { 5, 0, 0, 0, 0 },
                                      { 0, 2, 1, 4, 0 },
                                      { 0, 3, 0, 5, 0 },
                                      { 0, 0, 0, 0, 6 }};
+		String zwarteKaart1 = "src\\img\\cards\\level2\\caravan_onyx.jpg";
+		String zwarteKaart2 = "src\\img\\cards\\level2\\lapidary_onyx.jpg";
+		String[] zwarteKaartFotos = {zwarteKaart1, zwarteKaart1, zwarteKaart2, zwarteKaart1, zwarteKaart2, zwarteKaart2};
 		
 		for (int i = 0; i < witteKaartKosten.length; i++) {
 			int prestigePunten = prestigeLijst[i];
-			String foto = fotoLijst[i];
-			String exceptionFoto = fotoBuitenPatroon[i];
+
 			kleur = Kleur.WIT;
-			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, foto, witteKaartKosten[i]));
+			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, witteKaartFotos[i], witteKaartKosten[i]));
 			kleur = Kleur.ROOD;
-			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, foto, rodeKaartKosten[i]));
-			//extra conditie voor blauw: foto buiten patroon
+			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, rodeKaartFotos[i], rodeKaartKosten[i]));
 			kleur = Kleur.BLAUW;
-			boolean exception = (i==3 || i==4);
-			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, exception? exceptionFoto : foto, blauweKaartKosten[i]));
+			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, blauweKaartFotos[i], blauweKaartKosten[i]));
 			kleur = Kleur.GROEN;
-			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, foto, groeneKaartKosten[i]));
+			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, groeneKaartFotos[i], groeneKaartKosten[i]));
 			kleur = Kleur.ZWART;
-			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, foto, zwarteKaartKosten[i]));
+			niveau2.add(new Ontwikkelingskaart(2, prestigePunten, kleur, zwarteKaartFotos[i], zwarteKaartKosten[i]));
 			
 		}
 		return niveau2;
@@ -144,58 +169,74 @@ public class OntwikkelingskaartMapper {
 		Kleur kleur;
 		List<Ontwikkelingskaart> niveau3 = new ArrayList<>();
 		// niveau3
-		/*WIT,ROOD,BLAUW,GROEN,ZWART;*/
+		/*WIT,ROOD,BLAUW,GROEN,ZWART*/
+		/*DIAMOND, RUBY, SAPPHIRE, EMERALD, ONYX*/
 		int[] prestigeLijst = {3, 4, 4, 5};
 		String[] fotoLijstPatroonA = {"1", "1", "2", "2"};
 		String[] fotoLijstPatroonB = {"1", "2", "2", "1"};
 		
-		//FP B
+		/* WIT : DIAMOND  (patroon B)*/
 		int[][] witteKaartKosten = {{ 0, 5, 3, 3, 3 },
                                     { 0, 0, 0, 0, 7 },
                                     { 3, 3, 0, 0, 6 },
                                     { 3, 0, 0, 0, 7 }};
-		//FP A
+		String witteKaart1 = "src\\img\\cards\\level3\\building_diamond.jpg";
+		String witteKaart2 = "src\\img\\cards\\level3\\shop_diamond.jpg";
+		String[] witteKaartFotos = {witteKaart1, witteKaart2, witteKaart2, witteKaart1};
+
+		/* ROOD : RUBY  (patroon A) */
 		int[][] rodeKaartKosten = {{ 3, 0, 5, 3, 3 },
                                    { 0, 0, 0, 7, 0 },
                                    { 0, 3, 3, 6, 0 },
                                    { 0, 3, 0, 7, 0 }};
-		//FP A
+		String rodeKaart1 = "src\\img\\cards\\level3\\statue_ruby.jpg";
+		String rodeKaart2 = "src\\img\\cards\\level3\\building_ruby.jpg";
+		String[] rodeKaartFotos = {rodeKaart1, rodeKaart1, rodeKaart2, rodeKaart2};
+
+		/* BLAUW : SAPPHIRE (patroon A) */
 		int[][] blauweKaartKosten = {{ 3, 3, 0, 3, 5 },
                                      { 7, 0, 0, 0, 0 },
                                      { 6, 0, 3, 0, 3 },
                                      { 7, 0, 3, 0, 0 }};
-		//FP B
+		String blauweKaart1 = "src\\img\\cards\\level3\\canal_sapphire.jpg";
+		String blauweKaart2 = "src\\img\\cards\\level3\\shop_sapphire.jpg";
+		String[] blauweKaartFotos = {blauweKaart1, blauweKaart1, blauweKaart2, blauweKaart2};
+
+		/* GROEN : EMERALD (patroon B) */
 		int[][] groeneKaartKosten = {{ 5, 3, 3, 0, 3 },
                                      { 0, 0, 7, 0, 0 },
                                      { 3, 0, 6, 3, 0 },
                                      { 0, 0, 7, 3, 0 }};
-		//FP B
+		String groeneKaart1 = "src\\img\\cards\\level3\\building_emerald.jpg";
+		String groeneKaart2 = "src\\img\\cards\\level3\\bridge_emerald.jpg";
+		String[] groeneKaartFotos = {groeneKaart1, groeneKaart2, groeneKaart2, groeneKaart1};
+
+		/* ZWART : ONYX (patroon B) */
 		int[][] zwarteKaartKosten = {{ 3, 3, 3, 5, 0 },
                                      { 0, 7, 0, 0, 0 },
                                      { 0, 6, 0, 3, 3 },
                                      { 0, 7, 0, 0, 3 }};
-		//Twee lussen -> foto-patroon A//B
-		//patroon A (ROOD, BLAUW):
+		String zwarteKaart1 = "src\\img\\cards\\level3\\alley_onyx.jpg";
+		String zwarteKaart2 = "src\\img\\cards\\level3\\street_onyx.jpg";
+		String[] zwarteKaartFotos = {zwarteKaart1, zwarteKaart2, zwarteKaart2, zwarteKaart1};
+		
+		
 		for (int i = 0; i < witteKaartKosten.length; i++) {
-			String foto = fotoLijstPatroonA[i];
 			int prestigePunten = prestigeLijst[i];
+			
+			kleur = Kleur.WIT;
+			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, witteKaartFotos[i], witteKaartKosten[i]));
 			kleur = Kleur.ROOD;
-			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, foto, rodeKaartKosten[i]));
+			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, rodeKaartFotos[i], rodeKaartKosten[i]));
 			kleur = Kleur.BLAUW;
-			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, foto, blauweKaartKosten[i]));
+			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, blauweKaartFotos[i], blauweKaartKosten[i]));
+			kleur = Kleur.GROEN;
+			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, groeneKaartFotos[i], groeneKaartKosten[i]));
+			kleur = Kleur.ZWART;
+			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, zwarteKaartFotos[i], zwarteKaartKosten[i]));
 			
 		}
-		//patroon B (WIT, GROEN, ZWART):
-		for (int i = 0; i < witteKaartKosten.length; i++) {
-			String foto = fotoLijstPatroonB[i];
-			int prestigePunten = prestigeLijst[i];
-			kleur = Kleur.WIT;
-			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, foto, witteKaartKosten[i]));
-			kleur = Kleur.GROEN;
-			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, foto, groeneKaartKosten[i]));
-			kleur = Kleur.ZWART;
-			niveau3.add(new Ontwikkelingskaart(3, prestigePunten, kleur, foto, zwarteKaartKosten[i]));
-		}
+
 		return niveau3;
 	}
 
