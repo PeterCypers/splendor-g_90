d89db845d700150fd9a61d5ea8332bed4bb06551
Sterre Govaert <sterre.govaert@student.hogent.be>
2023-05-02 10:04:29 +0200

    improved the startups files

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -6,39 +6,39 @@ import java.util.Scanner;
 import cui.SplendorApplicatie;
 import domein.DomeinController;
 import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class StartUp extends Application {
+public abstract class StartUp extends Application {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int keuze = 0;
-		boolean errorExists = true;
+		try (Scanner input = new Scanner(System.in)) {
+			int keuze = 0;
+			boolean errorExists = true;
 
-		do {
-			try {
-				do {
-					System.out.print("Gui?(1) of Cui?(2): ");
-					keuze = input.nextInt();
-				} while (keuze < 1 || keuze > 2);
-				errorExists = false;
-			} catch (InputMismatchException e) {
-				input.next();
-				System.out.println("Je keuze moet een geheel getal zijn.");
+			do {
+				try {
+					do {
+						System.out.print("Gui?(1) of Cui?(2): ");
+						keuze = input.nextInt();
+					} while (keuze < 1 || keuze > 2);
+					errorExists = false;
+				} catch (InputMismatchException e) {
+					input.next();
+					System.out.println("Je keuze moet een geheel getal zijn.");
+				}
+			} while (errorExists);
+
+			// CUI opstarten
+			if (keuze == 2) {
+				new SplendorApplicatie(new DomeinController()).startSpel();
+				System.exit(0);
 			}
-		} while (errorExists);
-
-		// CUI opstarten
-		if (keuze == 2) {
-			new SplendorApplicatie(new DomeinController()).startSpel();
-			System.exit(0);
-		}
-		// GUI opstarten
-		if (keuze == 1) {
-			try {
-				launchGui(args);
-			} catch (Error e) {
-				System.exit(1);
+			// GUI opstarten
+			if (keuze == 1) {
+				try {
+					launchGui(args);
+				} catch (Error e) {
+					System.exit(1);
+				}
 			}
 		}
 	}
@@ -46,14 +46,4 @@ public class StartUp extends Application {
 	private static void launchGui(String[] args) {
 		Application.launch(StartUpGui.class, args);
 	}
-
-	private static void launchCui(String[] args) {
-		Application.launch(StartUpCui.class, args);
-	}
-
-	@Override
-	public void start(Stage arg0) throws Exception {
-		// Leave this method empty
-	}
-
 }
--- src/main/StartUpGui.java
+++ src/main/StartUpGui.java
@@ -1,9 +1,7 @@
 package main;
 
 import domein.DomeinController;
-
 import gui.TaalKeuzeScherm;
-import gui.WinnaarScherm;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -13,18 +11,18 @@ public class StartUpGui extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		DomeinController dc = new DomeinController();
-//		TaalKeuzeScherm taalKeuze = new TaalKeuzeScherm(dc);
-//		Scene scene = new Scene(taalKeuze, 800, 600);
-//		primaryStage.setScene(scene);
-//		primaryStage.setTitle("Taalkeuze");
-//		primaryStage.show();
-		
-		WinnaarScherm victory = new WinnaarScherm(dc);
-		Scene scene = new Scene (victory,800,600);
+		TaalKeuzeScherm taalKeuze = new TaalKeuzeScherm(dc);
+		Scene scene = new Scene(taalKeuze, 800, 600);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("victory");
+		primaryStage.setTitle("Taalkeuze");
 		primaryStage.show();
-		
+
+//		WinnaarScherm victory = new WinnaarScherm(dc);
+//		Scene scene = new Scene (victory,800,600);
+//		primaryStage.setScene(scene);
+//		primaryStage.setTitle("victory");
+//		primaryStage.show();
+
 	}
 
 	public static void main(String[] args) {
