d587ba7f1b6ddc6244cc1a41066e10e1d490d69a
Sterre Govaert <sterre.govaert@student.hogent.be>
2023-05-07 20:31:44 +0200

    startup gui

--- src/main/StartUpGui.java
+++ src/main/StartUpGui.java
@@ -1,7 +1,7 @@
 package main;
 
 import domein.DomeinController;
-import gui.SpeelSpelScherm;
+import gui.TaalKeuzeScherm;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,23 +10,23 @@ public class StartUpGui extends Application {
 
 	@Override
 	public void start(Stage stage) {
-//		DomeinController dc = new DomeinController();
-//		TaalKeuzeScherm taalKeuze = new TaalKeuzeScherm(dc);
-//		Scene scene = new Scene(taalKeuze);
-//		stage.setMaximized(true);
-//		stage.setScene(scene);
-//		stage.setTitle("Taalkeuze");
-//		stage.show();
-
 		DomeinController dc = new DomeinController();
-		dc.voegSpelerToe("user1", 2002);
-		dc.voegSpelerToe("user2", 2000);
-		SpeelSpelScherm speelSpelScherm = new SpeelSpelScherm(dc);
-		Scene scene = new Scene(speelSpelScherm);
+		TaalKeuzeScherm taalKeuze = new TaalKeuzeScherm(dc);
+		Scene scene = new Scene(taalKeuze);
 		stage.setMaximized(true);
 		stage.setScene(scene);
-		stage.setTitle("Spel");
+		stage.setTitle("Taalkeuze");
 		stage.show();
+
+//		DomeinController dc = new DomeinController();
+//		dc.voegSpelerToe("user1", 2002);
+//		dc.voegSpelerToe("user2", 2000);
+//		SpeelSpelScherm speelSpelScherm = new SpeelSpelScherm(dc);
+//		Scene scene = new Scene(speelSpelScherm);
+//		stage.setMaximized(true);
+//		stage.setScene(scene);
+//		stage.setTitle("Spel");
+//		stage.show();
 
 		// WinnaarScherm victory = new WinnaarScherm(dc);
 		// Scene scene = new Scene (victory,800,600);
