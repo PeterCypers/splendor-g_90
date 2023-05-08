38eaaf025423b93632f14ed289c48d7198d0fb7b
Peter Cypers <peter.cypers@student.hogent.be>
2023-05-08 06:34:45 +0200

    System.exit(0) opnieuw toegevoegd aan de CUI

--- src/main/StartUpCui.java
+++ src/main/StartUpCui.java
@@ -10,6 +10,7 @@ public class StartUpCui extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		new SplendorApplicatie(new DomeinController()).startSpel();
+		System.exit(0);
 	}
 
 	public static void main(String[] args) {
